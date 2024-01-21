6c1b6cb38a19d2f768cc6803311ff80c09d1ddf2
renzcarlo.glorioso <renzcarlo.glorioso@gmail.com>
2024-01-21 20:17:33 +0800

    Added S41 complete

--- backend/s41-s49/controllers/course.js
+++ backend/s41-s49/controllers/course.js
@@ -6,7 +6,7 @@ const auth = require("../auth");
 module.exports.getAllCourses = () => {
   return Course.find({})
     .then((result) => {
-      if (!result) {
+      if (result.length != 0) {
         return result;
       } else {
         return false;
@@ -15,6 +15,17 @@ module.exports.getAllCourses = () => {
     .catch((err) => err);
 };
 
-module.exports.addCourse = () => {
-  return Course;
+module.exports.addCourse = (reqBody) => {
+  let newCourse = new Course({
+    name: reqBody.name,
+    description: reqBody.description,
+    price: reqBody.price,
+    isActive: reqBody.isActive,
+    createdOn: reqBody.createdOn,
+  });
+
+  return newCourse
+    .save()
+    .then((result) => result)
+    .catch((err) => err);
 };
