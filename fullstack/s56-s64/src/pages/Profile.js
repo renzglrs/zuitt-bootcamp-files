51f420d923f9f466a48975da6debbf6d2e4d1d83
renzcarlo.glorioso <renzcarlo.glorioso@gmail.com>
2024-02-01 11:52:36 +0800

    Added S60 activity

--- fullstack/s56-s64/src/pages/Profile.js
+++ fullstack/s56-s64/src/pages/Profile.js
@@ -1,5 +1,5 @@
-import { useState, useEffect, useContext } from "react";
-import { Form, Button, Container, Nav } from "react-bootstrap";
+import { useContext } from "react";
+import { Container } from "react-bootstrap";
 import UserContext from "../UserContext";
 import { Navigate } from "react-router-dom";
 
@@ -11,6 +11,13 @@ export default function Profile() {
   ) : (
     <Container className="my-5 p-5 bg-primary text-white">
       <h1>Profile</h1>
+      <h2>John Smith</h2>
+      <div className="border border-bottom"></div>
+      <h3>Contacts</h3>
+      <ul>
+        <li>Email: jamesDC@mail.com</li>
+        <li>Mobile No: 09123456789</li>
+      </ul>
     </Container>
   );
 }
