2aaf3cd4bc1a3b9261d1b4e258be7071217ecf7b
renzcarlo.glorioso <renzcarlo.glorioso@gmail.com>
2024-02-06 15:43:20 +0800

    Added s66

--- fullstack/s66/activity/queue.js
+++ fullstack/s66/activity/queue.js
@@ -3,6 +3,52 @@ let collection = [];
 // Write the queue functions below.
 // Do not use built-in Array Methods.
 
+let firstIndex = 0;
+let lastIndex = 0;
+
+function print(){
+    return collection;
+}
+
+function enqueue(item){
+    collection[lastIndex] = item;
+    lastIndex++;
+    return collection;
+}
+
+function dequeue(){
+    const [firstElement, ...restOfArray] = collection;
+    collection = restOfArray;
+    lastIndex--;
+    return collection;
+}
+
+function front(){
+    const firstItem = collection[firstIndex];
+    return firstItem;
+}
+
+function size(){
+    const size = lastIndex - firstIndex;
+    return size;
+}
+
+function isEmpty() {
+    return firstIndex === lastIndex;
+}
+
+// console.log(print(collection));
+// console.log(enqueue('John'));
+// console.log(enqueue("Jane"));
+// console.log(dequeue());
+// console.log(enqueue("Bob"));
+// console.log(enqueue("Cherry"));
+// console.log(front());
+// console.log(size());
+// console.log(dequeue());
+// console.log(dequeue());
+// console.log(dequeue());
+// console.log(isEmpty());
 
 module.exports = {
     collection,
